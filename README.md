8c0424228ee581fa04c746ef0c65f98f74bab3f2
asgarzeeshan <asgarzeeshan@gmail.com>
2020-04-12 19:11:24 +0100

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Polls is a simple Django app to conduct Web-based polls. For each question, visitors can choose between a fixed number of answers.
+
+### This polls app is originally developed by https://github.com/do-community/django-polls
+
+### It is used to demonstrate the working of pytest with django.
--- polls/tests.py
+++ polls/tests.py
@@ -8,133 +8,106 @@ from .models import Question
 
 
 @pytest.mark.django_db
-def test_was_published_recently_with_future_question():
-    """
-    was_published_recently() returns False for questions whose pub_date
-    is in the future.
-    """
-    time = timezone.now() + datetime.timedelta(days=30)
-    future_question = Question(pub_date=time)
-    assert future_question.was_published_recently() is False
+def test_old_question_was_published_recently():
+    date = timezone.now() - datetime.timedelta(days=1, seconds=1)
+    question = Question(pub_date=date)
 
-
-@pytest.mark.django_db
-def test_was_published_recently_with_old_question():
-    """
-    was_published_recently() returns False for questions whose pub_date
-    is older than 1 day.
-    """
-    time = timezone.now() - datetime.timedelta(days=1, seconds=1)
-    old_question = Question(pub_date=time)
-    assert old_question.was_published_recently() is False
+    assert question.was_published_recently() is False
 
 
 @pytest.mark.django_db
 def test_was_published_recently_with_recent_question():
-    """
-    was_published_recently() returns True for questions whose pub_date
-    is within the last day.
-    """
-    time = timezone.now() - datetime.timedelta(hours=23, minutes=59, seconds=59)
-    recent_question = Question(pub_date=time)
-    assert recent_question.was_published_recently() is True
+    date = timezone.now() - datetime.timedelta(hours=23, minutes=59, seconds=59)
+    question = Question(pub_date=date)
+
+    assert question.was_published_recently() is True
 
 
-def create_question(question_text, days):
-    """
-    Create a question with the given `question_text` and published the
-    given number of `days` offset to now (negative for questions published
-    in the past, positive for questions that have yet to be published).
-    """
-    time = timezone.now() + datetime.timedelta(days=days)
-    return Question.objects.create(question_text=question_text, pub_date=time)
+@pytest.mark.django_db
+def test_future_question_was_published_recently():
+    date = timezone.now() + datetime.timedelta(days=30)
+    question = Question(pub_date=date)
+
+    assert question.was_published_recently() is False
+
+
+def create_questions_fixture(text, days):
+    date = timezone.now() + datetime.timedelta(days=days)
+    question_obj = Question.objects.create(question_text=text, pub_date=date)
+
+    return question_obj
 
 
 @pytest.mark.django_db
 def test_no_questions(client):
-    """
-    If no questions exist, an appropriate message is displayed.
-    """
     response = client.get(reverse('polls:index'))
+
     assert response.status_code == 200
+
     assert list(response.context['latest_question_list']) == []
 
 
 @pytest.mark.django_db
-def test_past_question(client):
-    """
-    Questions with a pub_date in the past are displayed on the
-    index page.
-    """
-    question_text = "Past question."
-    create_question(question_text=question_text, days=-30)
-    response = client.get(reverse('polls:index'))
-    assert response.status_code == 200
-    assert bool(response.context['latest_question_list']) is True
-    assert response.context['latest_question_list'][0].question_text == question_text
-
-
-@pytest.mark.django_db
 def test_future_question(client):
-    """
-    Questions with a pub_date in the future aren't displayed on
-    the index page.
-    """
-    create_question(question_text="Future question.", days=30)
-    response = client.get(reverse('polls:index'))
-    assert response.status_code == 200
-    assert list(response.context['latest_question_list']) == []
-
-
-@pytest.mark.django_db
-def test_future_question_and_past_question(client):
-    """
-    Even if both past and future questions exist, only past questions
-    are displayed.
-    """
-    past_question_text = "Past question."
-    create_question(question_text=past_question_text, days=-30)
-    create_question(question_text="Future question.", days=30)
-    response = client.get(reverse('polls:index'))
-    assert response.status_code == 200
-    assert bool(response.context['latest_question_list']) is True
-    assert response.context['latest_question_list'][0].question_text == past_question_text
-
-
-@pytest.mark.django_db
-def test_two_past_questions(client):
-    """
-    The questions index page may display multiple questions.
-    """
-    question_text_one = "Past question 1."
-    question_text_two = "Past question 2."
-    create_question(question_text=question_text_one, days=-30)
-    create_question(question_text=question_text_two, days=-5)
-    response = client.get(reverse('polls:index'))
-    assert response.status_code == 200
-    assert response.context['latest_question_list'][0].question_text == question_text_two
-    assert response.context['latest_question_list'][1].question_text == question_text_one
-
-
-@pytest.mark.django_db
-def test_future_question(client):
-    """
-    The detail view of a question with a pub_date in the future
-    returns a 404 not found.
-    """
-    future_question = create_question(question_text='Future question.', days=5)
-    url = reverse('polls:detail', args=(future_question.id,))
+    question = create_questions_fixture(text='some question', days=5)
+    url = reverse('polls:detail', args=(question.id,))
     response = client.get(url)
+
     assert response.status_code == 404
 
 
 @pytest.mark.django_db
-def test_past_question(client):
-    """
-    The detail view of a question with a pub_date in the past
-    displays the question's text.
-    """
-    past_question = create_question(question_text='Past Question.', days=-5)
+def test_question_past(client):
+    question_text = "some question"
+    create_questions_fixture(text=question_text, days=-30)
+    response = client.get(reverse('polls:index'))
+
+    assert bool(response.context['latest_question_list']) is True
+
+    assert response.context['latest_question_list'][0].question_text == question_text
+
+    assert response.status_code == 200
+
+
+@pytest.mark.django_db
+def test_question_future(client):
+    create_questions_fixture(text="Some question", days=30)
+    response = client.get(reverse('polls:index'))
+    assert response.status_code == 200
+    assert list(response.context['latest_question_list']) == []
+
+
+@pytest.mark.django_db
+def test_question_past(client):
+    past_question = create_questions_fixture(text='some Question.', days=-5)
     url = reverse('polls:detail', args=(past_question.id,))
     response = client.get(url)
+    assert response.status_code == 200
+
+
+@pytest.mark.django_db
+def test_two_questions_past(client):
+    question_text_one = "some question 1."
+    question_text_two = "some question 2."
+    create_questions_fixture(text=question_text_one, days=-30)
+    create_questions_fixture(text=question_text_two, days=-5)
+    response = client.get(reverse('polls:index'))
+    assert response.context['latest_question_list'][0].question_text == question_text_two
+
+    assert response.context['latest_question_list'][1].question_text == question_text_one
+
+    assert response.status_code == 200
+
+
+@pytest.mark.django_db
+def test_question_future_and_past_question(client):
+    question = "some question."
+    create_questions_fixture(text=question, days=-30)
+    create_questions_fixture(text="some other question", days=30)
+    response = client.get(reverse('polls:index'))
+
+    assert bool(response.context['latest_question_list']) is True
+
+    assert response.context['latest_question_list'][0].question_text == question
+
     assert response.status_code == 200
